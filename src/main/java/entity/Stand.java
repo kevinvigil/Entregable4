829a0ff96e7d6835782b50ba070015f0c0e53379
kevinVigil <kevinvigil27@gmail.com>
2023-10-25 15:06:52 -0300

    no message

--- src/main/java/entity/Stand.java
+++ src/main/java/entity/Stand.java
@@ -3,10 +3,14 @@ package entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.Set;
+
 @Entity
 @Data
 public class Stand {
     @Id
     private  Long id;
 
+    @OneToMany
+    private Set<Scooter> scooters;
 }
